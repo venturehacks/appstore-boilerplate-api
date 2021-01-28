39b192eec5d077977c6375b5022659a0a5941a53
Keith Connolly <streek@mesaro.com>
2021-01-28 12:56:50 -0700

    update hello world

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ app.get("/", (req, res) => {
   res.send("Hello World!!");
 });
 
+app.get("/preview", (req, res) => {
+  res.send("PREVIEW!!");
+});
+
+app.get("/results", (req, res) => {
+  res.send("RESULTS!!");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
