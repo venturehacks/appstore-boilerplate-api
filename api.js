a630b48b2fcbd3650cd20e97b0fc2e19f21fdbf2
Keith Connolly <streek@mesaro.com>
2021-03-03 10:24:39 -0700

    use real user_id

--- api.js
+++ api.js
@@ -1,26 +1,34 @@
+//Include fetch package so we can make node-side requests.
 const fetch = require("node-fetch");
+const apiKey = "lW57DHEAaqpCvCZott0rFA";
 
-const getToken = function () {
+// user_id = "11d09333-5d13-4881-a011-890a0787f044"
+
+//Auth our user and get a token in these examples we will use a predefined user_id and API key.
+//You should use yours as these will not work.
+const getToken = function (uid) {
   return fetch("https://angel.dev/appstore/api/auth", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
     }),
   })
     .then((res) => res.json()) // expecting a json response
     .then((json) => json.token);
 };
 
-const setCount = async function (token, value) {
+//Set count on the app-level storage, then add and set the user value on
+//the leaderboard which is vendor-level storage.
+const setCount = async function (token, value, uid) {
   await set(token, value, "11d09333-5d13-4881-a011-890a0787f044");
   return fetch("https://angel.dev/appstore/api/set", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
       key: "count",
       value,
       token,
@@ -30,13 +38,14 @@ const setCount = async function (token, value) {
     .then((json) => json.count);
 };
 
-const submit = async function (token, value) {
+//Submit the score to be displayed on angellist.
+const submit = async function (token, value, uid) {
   return fetch("https://angel.dev/appstore/api/submit", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
       value,
       token,
     }),
@@ -45,13 +54,14 @@ const submit = async function (token, value) {
     .then((json) => json.value);
 };
 
-const getCount = function (token) {
+//Get our key/value stored count saved on the app-level storage.
+const getCount = function (token, uid) {
   return fetch("https://angel.dev/appstore/api/get", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
       key: "count",
       token,
     }),
@@ -60,16 +70,18 @@ const getCount = function (token) {
     .then((json) => json.count);
 };
 
-const set = async function (token, value, user_id) {
-  const leaderboard = await get(token);
-  leaderboard[user_id] = value;
+//Warning, there could be a race condition here.  We get and then update the value of the leaderboard as a blob.
+//It's up to the developer to avoid these conditions for the vendor level storage.
+const set = async function (token, value, uid) {
+  const leaderboard = await get(token, uid);
+  leaderboard[uid] = value;
 
   return fetch("https://angel.dev/appstore/api/set", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
       vendor_level: true,
       key: "leaderboard",
       value: leaderboard,
@@ -80,13 +92,14 @@ const set = async function (token, value, user_id) {
     .then((json) => json.leaderboard);
 };
 
-const get = function (token) {
+//Gets the leaderboard from the vendor-level storage.
+const get = function (token, uid) {
   return fetch("https://angel.dev/appstore/api/get", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
-      api_key: "lW57DHEAaqpCvCZott0rFA",
-      user_id: "11d09333-5d13-4881-a011-890a0787f044",
+      api_key: apiKey,
+      user_id: uid,
       vendor_level: true,
       key: "leaderboard",
       token,
@@ -97,6 +110,7 @@ const get = function (token) {
       const leaderboard = json.leaderboard || {};
       const sortedLeaderboard = {};
 
+      //Here we sort the leaderboard hash by score values.
       var keys = Object.keys(leaderboard);
       keys
         .sort(function (a, b) {
@@ -111,4 +125,5 @@ const get = function (token) {
     });
 };
 
+//Export these functions...
 module.exports = { getCount, getToken, setCount, get, submit };
--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const port = process.env.PORT || 5555;
 
 //Loads the handlebars module
 const handlebars = require("express-handlebars");
-//Sets our app to use the handlebars engine
 app.set("view engine", "handlebars");
-//Sets handlebars configurations (we will go through them later on)
 app.engine(
   "handlebars",
   handlebars({
@@ -14,33 +12,39 @@ app.engine(
   })
 );
 
-let token = null;
-const { getToken, getCount, setCount, get, submit } = require("./api");
-
+//Allow use of public folder.
 app.use(express.static("public"));
+
+//Import actions from out api file.
+const { getToken, getCount, setCount, get, submit } = require("./api");
 
 //Required View Pages
 app.get("/main", async (req, res) => {
-  token = await getToken();
-  const count = await getCount(token);
-  console.log({ token, count });
+  const uid = req.query?.uid;
+  const token = await getToken(uid);
+  const count = await getCount(token, uid);
   res.render("main", { token, count });
 });
 
 app.get("/preview", (req, res) => {
+  const uid = req.query?.uid;
   res.render("preview");
 });
 
 app.get("/results", (req, res) => {
-  const leaderboard = get(token);
+  const uid = req.query?.uid;
+  const token = await getToken(uid);
+  const leaderboard = get(token, uid);
   res.render("results", { leaderboard });
 });
 
 //Post routes
 app.get("/i", async (req, res) => {
+  const uid = req.query?.uid;
+  const token = await getToken(uid);
   const count = req.query?.count;
-  setCount(token, count);
-  submit(token, count);
+  setCount(token, count, uid);
+  submit(token, count, uid);
   res.json({ success: true });
 });
 
